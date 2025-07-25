69d29946c44f9fa3fdccd474223a32117ccdaf6a
Jack McArthur <37667252+jackmcarthur@users.noreply.github.com>
2025-07-25 14:18:19 -0700

    Add test for JAX driver

---
+++ cohsex_jax.py
@@ -0,0 +1,9 @@
+from isdf.gw_isdf.cohsex_jax import main
+import os
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        os.chdir(os.path.join(os.path.dirname(__file__), "examples/cohsex_test"))
+        sys.argv += ["--input", "cohsex_test.in"]
+    raise SystemExit(main())
